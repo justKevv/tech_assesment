f0e8e2094aa329ffc3a75d0dd4431b28241dbec5
justKevv <kevinbramastaa@gmail.com>
2026-03-02 00:31:06 +0800

    refactor(database): remove redundant metadata creation in pipeline-service database config

--- pipeline-service/database.py
+++ pipeline-service/database.py
@@ -10,8 +10,6 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base.metadata.create_all(bind=engine)
-
 def get_db():
     db = SessionLocal()
     try:
